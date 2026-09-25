3fb564a761dae4f136537e66bac91a9134142a63
Hamberfim <adhamlin@gmail.com>
2026-09-25 12:47:12 -0500

    Hello and time

--- 01_basics/hello.go
+++ 01_basics/hello.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 
 	"rsc.io/quote"
+
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello, It's Go Time!")
 	fmt.Println(quote.Go())
+
+	fmt.Println("Welcome, the time is", time.Now().Format(time.UnixDate))
 }
